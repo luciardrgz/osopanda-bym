f5796e87b719b22f271a293dacfb368f3c68a7d5
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2024-03-16 13:07:30 -0300

    Products for dropdown

---
+++ app/consts/products.js
@@ -0,0 +1,44 @@
+import {
+  ShoppingBagIcon,
+  SparklesIcon,
+  HeartIcon,
+  SunIcon,
+  DevicePhoneMobileIcon,
+  ListBulletIcon,
+} from "@heroicons/react/20/solid";
+
+export const products = [
+  {
+    name: "Bolsos",
+    description: "Playeros, materos, etc.",
+    href: "#",
+    icon: ShoppingBagIcon,
+  },
+  {
+    name: "Mochilas",
+    description: "Para la escuela, el trabajo, etc.",
+    href: "#",
+    icon: SparklesIcon,
+  },
+  {
+    name: "Accesorios para el día a día",
+    description: "Portalentes, portaauriculares, etc.",
+    href: "#",
+    icon: HeartIcon,
+  },
+  {
+    name: "Verano",
+    description: "Lonas, bolsillos para reposeras, etc.",
+    href: "#",
+    icon: SunIcon,
+  },
+];
+
+export const callsToAction = [
+  { name: "Ver todo", href: "#", icon: ListBulletIcon },
+  {
+    name: "Ver en Instagram",
+    href: "#",
+    icon: DevicePhoneMobileIcon,
+  },
+];
